6c7f36c524d5e7dc90101800e812f78d9655dcd5
Nikita Prianichnikov <lpenguin@yandex.ru>
2012-04-20 14:48:01 +0400

    added info button

--- coffee/mnplayer.coffee
+++ coffee/mnplayer.coffee
@@ -8,33 +8,53 @@ Root.App =
 App.Settings = 
     barLength: 28
     autoLoad: true
+    messageStep: 500
+    messageSpace: 4
     
 App.Templates.Player = """
-    <div class="play-button symbol">|>&nbsp;</div>
-    <div class="pause-button hidden symbol">||&nbsp;</div>
+    <div class="play-button symbol clickable">|>&nbsp;</div>
+    <div class="pause-button hidden symbol clickable">||&nbsp;</div>
     <div class="symbol">[</div>
-    <div class="seek-bar symbol"></div>
+    <div class="seek-bar symbol clickable"></div>
     <div class="symbol">&nbsp;</div>
     <div class="time symbol"></div>
     <div class="symbol">]</div>
     &nbsp;
-    <div class="symbol home-page">[?]</div><br/>
+    <div class="symbol info clickable">[i]</div>
+    <div class="symbol home-page clickable">[?]</div><br/>
 """
 
 Root.mnplayer = ( ) ->
+    message = 'sorry HTML5 is not supported by your browser' if not buzz.isSupported()
     $('.mnplayer').each (i, ob) ->
+            
         url = $(ob).attr 'url'
+        mp3url = $(ob).attr 'mp3url'
+        oggurl = $(ob).attr 'oggurl'
+        
+        if mp3url and buzz.isMP3Supported()
+            url = mp3url
+        
+        if oggurl and buzz.isOGGSupported()
+            url = oggurl
+        if not url 
+            message = 'sorry, no audio'
         duration = $(ob).attr 'duration'
+        info = $(ob).attr 'info'
+        
         audio = new buzz.sound url, { preload:true, loop:false}
-        #console.log audio.getDuration()
-        player = new App.Views.Player model: audio, duration: duration 
+
+        player = new App.Views.Player model: audio, duration: duration, info: info, message: message
         $(ob).append player.render()
     
 buzzBind = (sound, event, func, obj) ->
-    f = () ->
-        func.call obj, this
+    f = (e) ->
+        func.call obj, e, this
     sound.bind event, f
-    
+replaceAll = (string, search, replace) ->
+    string.split(search).join(replace)
+
+
 App.Views.Player = Backbone.View.extend
     initialize: (options) ->
         @barLength = App.Settings.barLength;
@@ -42,30 +62,80 @@ App.Views.Player = Backbone.View.extend
             @barLength = options.barLength
         buzzBind @model, 'timeupdate', @timeupdate, this
         buzzBind @model, 'durationchange', @durationchange, this
-        
-        #@model.parentView = this
-        
-        #@model.bind('timeupdate', @timeupdate)
-        #@model.bind 'durationchange', @durationchange
+        #buzzBind @model, 'abort', @soundAbort, this
+        buzzBind @model, 'error', @soundError, this
         if options.duration
             @manualDuration = buzz.fromTimer options.duration
-            
-    timeupdate: (sound) ->
+        @info = options.info or ''
+        @showMode = 'bar'
+        @messagePosition = 0
+        @showMessage options.message if options.message
+        
+    setBarMode: () ->
+        @showMode = 'bar'
+        clearInterval @messageInterval if @messageInterval
+    setMessageMode: () ->
+        @showMode = 'message'
+        @messagePosition = 0
+        that = this
+        @messageInterval = setInterval( (() -> that.updateMessage()), App.Settings.messageStep)
+    updateMessage: () ->
+        @drawMessage()
+        @messagePosition++
+        @messagePosition = 0 if @messagePosition >= @message.length + App.Settings.messageSpace
+        
+    drawSeekBar: () ->
         @$el.find('.seek-bar').html @makeSeekBar()
         @$el.find('.time').html buzz.toTimer @model.getTime()
+    drawMessage: () ->
+        @$el.find('.seek-bar').html @makeMessageBar()
+        @$el.find('.time').html buzz.toTimer @model.getTime()
+    
+    timeupdate: (e) ->
+        if @showMode == 'bar'
+            @drawSeekBar()
+        if @showMode == 'message'
+            @drawMessage()
+            
         if @duration and @model.getTime() > @duration
             @pause()
             @model.setTime(0)
             
-    durationchange: (sound) ->
+    durationchange: (e) ->
         @duration = @model.getDuration()
         if @manualDuration
             @duration = @manualDuration
+    
+    showMessage: (message) ->
+        @message = message
+        @setMessageMode()
+        @drawMessage()
+    
+    soundAbort: (e) ->
+        @showMessage 'aborted'
+        
+    soundError: (e) ->
+        msg = 'error'
+        if e.target instanceof HTMLAudioElement
+            switch @model.getErrorCode()
+                when 1
+                    msg += ': aborted by user'
+                when 2 
+                    msg += ': network error'
+                when 3 
+                    msg += ': decode error'
+                when 4 
+                    msg += ': audio is not supported by your browser'
+        else if e.target instanceof HTMLSourceElement
+            msg += ': audio is not supported by your browser'
+                    
+        @showMessage msg
     events: 
         "click .play-button": "play"
         "click .pause-button": "pause"
         "click .seek-bar": "seek"
         "click .home-page": "homePage"
+        "click .info": "showInfo"
     
     template: App.Templates.Player
     className: "player-js"
@@ -73,11 +143,33 @@ App.Views.Player = Backbone.View.extend
         position = Math.floor @barLength * @model.getTime() / @duration
         out = ""
         for i in [0..@barLength]
-            if i <= position
+            if i < position
                 out += '='
             else
                 out += '-'
         return out
+        
+    makeMessageBar: () ->
+        space = App.Settings.messageSpace
+        m = minit = (@message or '') + Array(space+1).join(' ')
+
+        if m.length > @barLength
+            m = m.substring @messagePosition, @barLength + @messagePosition + 1
+
+        rest = @barLength - m.length + 1
+        
+        if minit.length > @barLength
+            left = minit.substring(0, rest)
+        else
+            left = Array(rest+1).join(' ')
+
+        return replaceAll m + left, ' ', '&nbsp;'
+    showInfo: () ->
+        if @showMode == 'message'
+            @setBarMode()
+            @drawSeekBar()
+            return
+        @showMessage @info
     render: () ->
         @$el.html @template
         @$el.find('.seek-bar').html @makeSeekBar()
@@ -93,6 +185,10 @@ App.Views.Player = Backbone.View.extend
         @$el.find('.play-button').toggleClass 'hidden'
         @$el.find('.pause-button').toggleClass 'hidden'
     seek: (e) ->
+        if @showMode == 'message'
+            @setBarMode()
+            @drawSeekBar()
+            return
         x = e.pageX - e.target.offsetLeft;
         width = $(e.target).width()
         @model.setTime x/width * @duration
--- js/mnplayer.js
+++ js/mnplayer.js
@@ -1,5 +1,5 @@
 (function() {
-  var Root, buzzBind;
+  var Root, buzzBind, replaceAll;
 
   Root = window;
 
@@ -12,23 +12,37 @@
 
   App.Settings = {
     barLength: 28,
-    autoLoad: true
+    autoLoad: true,
+    messageStep: 500,
+    messageSpace: 4
   };
 
-  App.Templates.Player = "<div class=\"play-button symbol\">|>&nbsp;</div>\n<div class=\"pause-button hidden symbol\">||&nbsp;</div>\n<div class=\"symbol\">[</div>\n<div class=\"seek-bar symbol\"></div>\n<div class=\"symbol\">&nbsp;</div>\n<div class=\"time symbol\"></div>\n<div class=\"symbol\">]</div>\n&nbsp;\n<div class=\"symbol home-page\">[?]</div><br/>";
+  App.Templates.Player = "<div class=\"play-button symbol clickable\">|>&nbsp;</div>\n<div class=\"pause-button hidden symbol clickable\">||&nbsp;</div>\n<div class=\"symbol\">[</div>\n<div class=\"seek-bar symbol clickable\"></div>\n<div class=\"symbol\">&nbsp;</div>\n<div class=\"time symbol\"></div>\n<div class=\"symbol\">]</div>\n&nbsp;\n<div class=\"symbol info clickable\">[i]</div>\n<div class=\"symbol home-page clickable\">[?]</div><br/>";
 
   Root.mnplayer = function() {
+    var message;
+    if (!buzz.isSupported()) {
+      message = 'sorry HTML5 is not supported by your browser';
+    }
     return $('.mnplayer').each(function(i, ob) {
-      var audio, duration, player, url;
+      var audio, duration, info, mp3url, oggurl, player, url;
       url = $(ob).attr('url');
+      mp3url = $(ob).attr('mp3url');
+      oggurl = $(ob).attr('oggurl');
+      if (mp3url && buzz.isMP3Supported()) url = mp3url;
+      if (oggurl && buzz.isOGGSupported()) url = oggurl;
+      if (!url) message = 'sorry, no audio';
       duration = $(ob).attr('duration');
+      info = $(ob).attr('info');
       audio = new buzz.sound(url, {
         preload: true,
         loop: false
       });
       player = new App.Views.Player({
         model: audio,
-        duration: duration
+        duration: duration,
+        info: info,
+        message: message
       });
       return $(ob).append(player.render());
     });
@@ -36,10 +50,14 @@
 
   buzzBind = function(sound, event, func, obj) {
     var f;
-    f = function() {
-      return func.call(obj, this);
+    f = function(e) {
+      return func.call(obj, e, this);
     };
     return sound.bind(event, f);
+  };
+
+  replaceAll = function(string, search, replace) {
+    return string.split(search).join(replace);
   };
 
   App.Views.Player = Backbone.View.extend({
@@ -48,27 +66,89 @@
       if (options.barLength) this.barLength = options.barLength;
       buzzBind(this.model, 'timeupdate', this.timeupdate, this);
       buzzBind(this.model, 'durationchange', this.durationchange, this);
-      if (options.duration) {
-        return this.manualDuration = buzz.fromTimer(options.duration);
+      buzzBind(this.model, 'error', this.soundError, this);
+      if (options.duration) this.manualDuration = buzz.fromTimer(options.duration);
+      this.info = options.info || '';
+      this.showMode = 'bar';
+      this.messagePosition = 0;
+      if (options.message) return this.showMessage(options.message);
+    },
+    setBarMode: function() {
+      this.showMode = 'bar';
+      if (this.messageInterval) return clearInterval(this.messageInterval);
+    },
+    setMessageMode: function() {
+      var that;
+      this.showMode = 'message';
+      this.messagePosition = 0;
+      that = this;
+      return this.messageInterval = setInterval((function() {
+        return that.updateMessage();
+      }), App.Settings.messageStep);
+    },
+    updateMessage: function() {
+      this.drawMessage();
+      this.messagePosition++;
+      if (this.messagePosition >= this.message.length + App.Settings.messageSpace) {
+        return this.messagePosition = 0;
       }
     },
-    timeupdate: function(sound) {
+    drawSeekBar: function() {
       this.$el.find('.seek-bar').html(this.makeSeekBar());
-      this.$el.find('.time').html(buzz.toTimer(this.model.getTime()));
+      return this.$el.find('.time').html(buzz.toTimer(this.model.getTime()));
+    },
+    drawMessage: function() {
+      this.$el.find('.seek-bar').html(this.makeMessageBar());
+      return this.$el.find('.time').html(buzz.toTimer(this.model.getTime()));
+    },
+    timeupdate: function(e) {
+      if (this.showMode === 'bar') this.drawSeekBar();
+      if (this.showMode === 'message') this.drawMessage();
       if (this.duration && this.model.getTime() > this.duration) {
         this.pause();
         return this.model.setTime(0);
       }
     },
-    durationchange: function(sound) {
+    durationchange: function(e) {
       this.duration = this.model.getDuration();
       if (this.manualDuration) return this.duration = this.manualDuration;
+    },
+    showMessage: function(message) {
+      this.message = message;
+      this.setMessageMode();
+      return this.drawMessage();
+    },
+    soundAbort: function(e) {
+      return this.showMessage('aborted');
+    },
+    soundError: function(e) {
+      var msg;
+      msg = 'error';
+      if (e.target instanceof HTMLAudioElement) {
+        switch (this.model.getErrorCode()) {
+          case 1:
+            msg += ': aborted by user';
+            break;
+          case 2:
+            msg += ': network error';
+            break;
+          case 3:
+            msg += ': decode error';
+            break;
+          case 4:
+            msg += ': audio is not supported by your browser';
+        }
+      } else if (e.target instanceof HTMLSourceElement) {
+        msg += ': audio is not supported by your browser';
+      }
+      return this.showMessage(msg);
     },
     events: {
       "click .play-button": "play",
       "click .pause-button": "pause",
       "click .seek-bar": "seek",
-      "click .home-page": "homePage"
+      "click .home-page": "homePage",
+      "click .info": "showInfo"
     },
     template: App.Templates.Player,
     className: "player-js",
@@ -77,13 +157,36 @@
       position = Math.floor(this.barLength * this.model.getTime() / this.duration);
       out = "";
       for (i = 0, _ref = this.barLength; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
-        if (i <= position) {
+        if (i < position) {
           out += '=';
         } else {
           out += '-';
         }
       }
       return out;
+    },
+    makeMessageBar: function() {
+      var left, m, minit, rest, space;
+      space = App.Settings.messageSpace;
+      m = minit = (this.message || '') + Array(space + 1).join(' ');
+      if (m.length > this.barLength) {
+        m = m.substring(this.messagePosition, this.barLength + this.messagePosition + 1);
+      }
+      rest = this.barLength - m.length + 1;
+      if (minit.length > this.barLength) {
+        left = minit.substring(0, rest);
+      } else {
+        left = Array(rest + 1).join(' ');
+      }
+      return replaceAll(m + left, ' ', '&nbsp;');
+    },
+    showInfo: function() {
+      if (this.showMode === 'message') {
+        this.setBarMode();
+        this.drawSeekBar();
+        return;
+      }
+      return this.showMessage(this.info);
     },
     render: function() {
       this.$el.html(this.template);
@@ -105,6 +208,11 @@
     },
     seek: function(e) {
       var width, x;
+      if (this.showMode === 'message') {
+        this.setBarMode();
+        this.drawSeekBar();
+        return;
+      }
       x = e.pageX - e.target.offsetLeft;
       width = $(e.target).width();
       return this.model.setTime(x / width * this.duration);
